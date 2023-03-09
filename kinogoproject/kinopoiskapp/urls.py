1cb0c7e145d721782785c4ac6b2b3c1f604eda98
Asmodeyad <liligame5@list.ru>
2023-03-09 16:06:07 +0600

    first commit

---
+++ kinogoproject/kinopoiskapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homePage, name='homePage'),
+    path('movies/top/', views.topMovies, name='topMovies'),
+    path('movies/all/', views.allMovies, name='allMovies'),
+    path('movies/all/filter/<slug:slug>/', views.allMovies, name='filterMovies'),
+    path('movie/detail/<int:pk>/', views.movieDetail, name='movieDetail'),
+    path('user/sign-up', views.signUp, name='signUp'),
+    path('user/login/', views.loginPage, name='loginPage'),
+    path('user/logout/', views.logoutUser, name='logoutUser'),
+    path('user/admin/add-movie', views.addMovie, name='addMovie'),
+    path('user/admin/add-genre', views.addGenre, name='addGenre'),
+]
